1e6eb0db2897f5e1dfbbe06133e928816f115e3a
Miron Bury <miron.bury@gmail.com>
2021-04-27 20:15:12 +0100

    move folders Add Proximity script

---
+++ Assets/Miron/Scripts/Proximity.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Proximity : MonoBehaviour
+{
+    [SerializeField]
+    private bool vanishOnStart = false;
+
+    void Start()
+    {
+        EnableChildren(!vanishOnStart);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        EnableChildren(vanishOnStart);
+    }
+    
+    void OnTriggerExit(Collider other)
+    {
+        EnableChildren(!vanishOnStart);
+    }
+
+    void EnableChildren(bool enable)
+    {
+        foreach (Transform child in transform)
+            child.gameObject.SetActive(enable);
+    }
+}
