0a0c1e7fe8272c5a04b5829ddb8ec56935c3804a
Miron Bury <miron.bury@gmail.com>
2021-04-28 01:37:58 +0100

    pause game on menu

---
+++ Assets/Miron/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager
+{
+    private static GameManager instance;
+
+    private bool paused = false;
+
+    private GameManager()
+    {
+
+    }
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameManager();
+            }
+
+            return instance;
+        }
+    }
+
+    public void OnApplicationQuit()
+    {
+        instance = null;
+    }
+
+    public void Pause(bool pause)
+    {
+        paused = pause;
+        Cursor.visible = pause;
+        Cursor.lockState = pause ? CursorLockMode.None : CursorLockMode.Locked;
+        Time.timeScale = pause ? 0 : 1;
+    }
+
+    public bool IsPaused()
+    {
+        return paused;
+    }
+}
---
+++ Assets/Miron/Scripts/Journal.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Journal : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject menu;
+    private bool axisInUse = false;
+
+    private void Update()
+    {
+        float axis = Input.GetAxisRaw("Journal");
+        if (!axisInUse && axis > 0)
+        {
+            axisInUse = true;
+            ToggleJournal();
+        }
+        else if (axis == 0)
+        {
+            axisInUse = false;
+        }
+    }
+
+    void ToggleJournal()
+    {
+        bool show = !menu.activeSelf;
+
+        menu.SetActive(show);
+        GameManager.Instance.Pause(show);
+    }
+
+    void OnGUI()
+    {
+        //Cursor.visible = true;
+    }
+}
